6ad0ed705307364777e2c152f6c11d1759923ae3
Marce Dalpra <marcelaldalpra@gmail.com>
2020-03-21 11:21:57 -0300

    theme + app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+      private platform: Platform,
+      private splashScreen: SplashScreen,
+      private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (this.platform.is('ios')) {
+        this.statusBar.styleDefault();
+      }
     });
   }
 }
