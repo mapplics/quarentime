fa5d7b81e3aa5700c9c9915b06afe93f312c9e29
Marce Dalpra <marcelaldalpra@gmail.com>
2020-03-22 12:49:47 -0300

    personal data

--- src/app/pages/personal-data/header/header.component.ts
+++ src/app/pages/personal-data/header/header.component.ts
@@ -3,6 +3,7 @@ import {PageInterface} from '../../../core/page-interface';
 import {locale as english} from './i18n/en';
 import {locale as spanish} from './i18n/es';
 import {TranslateService} from '@ngx-translate/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -12,17 +13,35 @@ import {TranslateService} from '@ngx-translate/core';
 export class HeaderComponent extends PageInterface implements OnInit {
 
   @Input() title: string;
-  @Input() stepsCompleted: number = 1;
+  @Input() currentStep: number;
+  @Input() completedSteps: number[] = [];
 
-  constructor(public translateService: TranslateService) {
+  constructor(public translateService: TranslateService,
+              private navController: NavController) {
     super(translateService, english, spanish);
   }
 
   ngOnInit() {}
 
   isCompleted(step): boolean {
-    return this.stepsCompleted >= step;
+    return this.completedSteps.includes(step);
   }
 
-  navigateTo(): void {}
+  isCurrent(step): boolean {
+    return this.currentStep === step;
+  }
+
+  getColor(step): string {
+    if (this.isCompleted(step)) {
+      return 'completed';
+    } else if (this.isCurrent(step)) {
+      return 'active';
+    } else {
+      return '';
+    }
+  }
+
+  navigateTo(page: string): void {
+    this.navController.navigateRoot('personal-data/' + page);
+  }
 }
