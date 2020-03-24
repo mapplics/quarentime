8db3b094b6b7d5fa1852cea529d83e73f60ed4dd
natali <nati6029@gmail.com>
2020-03-23 23:24:18 -0300

    add icons tabs and fixes

---
+++ src/app/pages/main/add-friends/add-friends.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-friends',
+  templateUrl: './add-friends.page.html',
+  styleUrls: ['./add-friends.page.scss'],
+})
+export class AddFriendsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
