383e18f2b9f56f00b9bb29051ca7c43982c361ef
Leilen <leilen.avila@mapplics.com>
2020-03-23 21:58:46 -0300

    home

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {locale as english} from './i18n/en';
+import {locale as spanish} from './i18n/es';
+import {TranslateService} from '@ngx-translate/core';
+import {PageInterface} from '../../../core/page-interface';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage extends PageInterface implements OnInit {
+   slideOpts = {
+    slidesPerView: 2,
+  };
 
-  constructor() { }
+  constructor(public translateService: TranslateService,
+              ) {
+    super(translateService, english, spanish);
+  }
 
   ngOnInit() {
+    // console.log(this.homeService.itemsInformationList);
   }
 
 }
--- src/app/pages/onboarding/onboarding.page.ts
+++ src/app/pages/onboarding/onboarding.page.ts
@@ -3,6 +3,7 @@ import {TranslateService} from '@ngx-translate/core';
 import {PageInterface} from '../../core/page-interface';
 import {locale as english} from './i18n/en';
 import {locale as spanish} from './i18n/es';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-onboarding',
@@ -11,7 +12,9 @@ import {locale as spanish} from './i18n/es';
 })
 export class OnboardingPage  extends PageInterface implements OnInit {
 
-  constructor(public translateService: TranslateService, ) {
+  constructor(public translateService: TranslateService,
+              private router: Router,
+              private homeService: HomeService) {
 
     super(translateService, english, spanish);
   }
@@ -20,7 +23,9 @@ export class OnboardingPage  extends PageInterface implements OnInit {
   }
 
 
-  goToHome() {
 
+
+  goToHome() {
+    this.router.navigate(['', 'main', 'home']);
   }
 }
