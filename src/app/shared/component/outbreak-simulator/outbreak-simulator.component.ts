5f547ef3ec6a19f9c310d4910c0c22a0d90ac766
Marce Dalpra <marcelaldalpra@gmail.com>
2020-04-15 10:55:22 -0300

    fix

--- src/app/shared/component/outbreak-simulator/outbreak-simulator.component.ts
+++ src/app/shared/component/outbreak-simulator/outbreak-simulator.component.ts
@@ -40,6 +40,5 @@ export class OutbreakSimulatorComponent implements OnInit, OnDestroy {
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.restore();
-    document.removeChild(this.canvas);
   }
 }
